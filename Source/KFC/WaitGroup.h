7ab6473a457e82c3e82360d3a91da4c14eda9415
mivinci <mivinci@qq.com>
2025-10-31 20:57:13 +0800

    KFC: complete WaitGroup

--- Source/KFC/WaitGroup.h
+++ Source/KFC/WaitGroup.h
@@ -2,28 +2,48 @@
 #include "KFC/Condition.h"
 #include "KFC/Mutex.h"
 
-namespace KFC {
+KFC_NAMESPACE_BEG
+
+// WaitGroup is a synchronization primitive that allows multiple threads to wait for a
+// collection of operations to finish.
+//
+// Example:
+//   WaitGroup wg;
+//   wg.add(1);
+//   Thread t([&] {
+//     // do something
+//     wg.done();
+//   });
+//   wg.wait();
+//
 class WaitGroup {
 public:
   explicit WaitGroup() : m_count(0) {}
   explicit WaitGroup(const int n) : m_count(0) { add(n); }
 
+  // Returns the number of pending operations.
+  int get() { return *m_count.lock(); }
+
+  // Adds `n` pending operations.
   void add(const int n) { *m_count.lock() += n; }
 
+  // Marks one pending operation as done.
   void done() {
     (*m_count.lock())--;
-    m_condvar.notifyOne();
+    m_condition.notifyOne();
   }
 
+  // Waits for all pending operations to be done.
   void wait(const Duration &timeout = Duration::FOREVER) {
     auto count_locked = m_count.lock();
     while (*count_locked > 0) {
-      m_condvar.wait(count_locked, timeout);
+      m_condition.wait(count_locked, timeout);
     }
   }
 
 private:
   Mutex<int> m_count;
-  Condition m_condvar;
+  Condition m_condition;
 };
-} // namespace KFC
+
+KFC_NAMESPACE_END
